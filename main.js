d347cd8ca5f0107c80e822ec0e94c530bac7ef45
Ramon Audain <raudain123@gmail.com>
2020-05-12 23:26:11 +0000

    Solution #1

---
+++ main.js
@@ -0,0 +1,15 @@
+function reverseInt(num){
+  // parseInt() turns a string into a number
+
+  // Solution #1
+  const reversed = num.toString().split("").reverse().join("")
+
+  return parseInt(reversed) * Math.sign(num);
+}
+
+
+// reverseInt(15);
+// reverseInt(189);
+// reverseInt(500);
+reverseInt(-51);
+// reverseInt(-90);
