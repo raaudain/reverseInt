f7c425d11c833df0fd68e032e9985d2a80ca2261
Ramon Audain <raudain123@gmail.com>
2020-05-13 00:39:05 +0000

    Solution #2

--- main.js
+++ main.js
@@ -7,14 +7,24 @@ function reverseInt(num){
   // return parseInt(reversed) * Math.sign(num);
 
 
-  // Solution # 1.5
-  return parseInt(num.toString().split("").reverse().join("")) * Math.sign(num);
-  
+  // // Solution #1.5
+  // return parseInt(num.toString().split("").reverse().join("")) * Math.sign(num);
+
+
+  // Solution #2
+  let reversed = "";
+  const numbers = num.toString().split("");
+
+  for (number of numbers){
+    reversed = number + reversed
+  }
+
+  return num < 0 ? parseInt(reversed) * -1 : parseInt(reversed); 
 }
 
 
 // reverseInt(15);
-// reverseInt(189);
+reverseInt(189);
 // reverseInt(500);
-reverseInt(-51);
+// reverseInt(-51);
 // reverseInt(-90);
